8090a51f3c2832fd57713aae60d6054021b85934
mike <mike58072@gmail.com>
2017-02-23 08:35:42 -0600

    uno

--- site/js/primero.js
+++ site/js/primero.js
@@ -1,2 +1,2 @@
-alert ("Bienvenido, te informamos que tenemos nuevo contenido agregado al sitio");
+alert ("Bienvenido, te informamos que tenemos nuevo contenido agregado al sitio, revisa la pestaña de contenidos ");
 setTimeout("alert('Hey hemos agreado un tutorial sobre como instalar ubuntu')", 3000);
