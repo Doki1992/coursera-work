6348b8cd4554c0d07c0558f843cf1ec25f6293b6
mike <mike58072@gmail.com>
2017-02-25 06:53:43 -0600

    f

--- isabel/js/isa.js
+++ isabel/js/isa.js
@@ -1,1 +1,42 @@
-alert("hola, no creo que estes viendo esto pero igual por si lo haces quiero decirte que Te amo enserio que lo hago como me hubiera gustado quedarme contigo pero tu no quisiste y pues bueno no puedo estar rogante siempre te vas a aburrir. jajaja te amo Isa con tigo se va una parte de mi");
+alert("Hola, quiero decierte que te amo. Tengo ganas de llorar  lo siento no me gusta que te enojes, perdon por solo decirte estupideces lo siento de verdad.");
+
+
+function agregar_elemento() {
+
+	var uno  = get(4);
+	alert(uno);
+	var padre = document.getElementById();
+	var nodo = document.createElement('div');
+	var texto = document.createTextNode("hola este es un texto de prueba");
+	nodo.appendChild(texto);
+	var cuerpo = document.getElementById("cuerpo");		
+	cuerpo.appendChild(nodo);
+}
+
+var contador = 0;
+
+function addi(){
+	var padre = document.getElementById("imagen");
+	padre.setAttribute('src',uno[get(++contador)]);
+	alert(contador);
+}
+
+function addr(){
+	var padre = document.getElementById("imagen");
+	padre.setAttribute('src',uno[get(--contador)]);
+	alert(contador);
+}
+
+var uno = ["images/uno.jpeg", "images/dos.jpeg", "images/tres.jpeg", "images/cuatro.jpeg","images/levi.png"];
+
+function get(numero){
+ if(numero>3){
+ 	contador = 0;
+ 	return  0;
+ }
+ if (numero<0) {contador = 3;return 3}
+
+ 	
+ return numero;
+}
+
