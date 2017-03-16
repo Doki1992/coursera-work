f9edd093b9816bbc97d829890213a130273891ce
mike <mike58072@gmail.com>
2017-03-16 07:12:48 -0600

    3

--- site/js/primero.js
+++ site/js/primero.js
@@ -1,2 +1,49 @@
-alert ("Bienvenido, te informamos que tenemos nuevo contenido agregado al sitio, revisa la pestaña de contenidos ");
-setTimeout("alert('Hey hemos agreado un tutorial sobre como instalar ubuntu')", 3000);
+function cambiar_estado(quien) {
+	var puntero = document.getElementById(quien);
+	puntero.setAttribute('class','active');
+	desactivar(quien);
+}
+
+function desactivar(quien) {
+	cosas = ["si","ti","pi"]
+	for (var i = cosas.length - 1; i >= 0; i--) {
+		if (quien != cosas[i]) {
+			var puntero = document.getElementById(cosas[i]);
+			puntero.setAttribute('class','');	
+		}	
+	}	
+}
+var x;
+x=$(document);
+x.ready(inicializarEventos);
+
+function inicializarEventos()
+{
+  var x=$("#si");
+  x.click(ocultar_recuadro);
+  x=$("#ti");
+  x.click(mostrar_cuadro);
+  x=$("#pi");
+  x.click(mostrar_cuadro);
+}
+
+function mostrar_cuadro(quien) {
+	
+	var cont = "";
+	if (quien == "si") {
+		cont="información sobre el segundo informe";	
+		alert(cont)	;
+	}else if (quien=="ti") {
+		cont="información sobre el  tercero informe";		
+		alert(cont)	;
+	}else{
+		cont="información sobre el primera informe";
+		alert(cont)	;
+	}
+	  
+}
+
+function ocultar_recuadro() {
+	 var x=$("#cuadro");
+     x.hide("slow")	
+}
